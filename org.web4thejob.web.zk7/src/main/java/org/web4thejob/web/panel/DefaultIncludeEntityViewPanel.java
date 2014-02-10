147462b39c768f4548200f639e8f03c26aa0868c
bissaias <bissaias@hotmail.com>
2014-02-10 21:30:42 +0200

    MVVM Enhancements (commands)

--- org.web4thejob.web.zk7/src/main/java/org/web4thejob/web/panel/DefaultIncludeEntityViewPanel.java
+++ org.web4thejob.web.zk7/src/main/java/org/web4thejob/web/panel/DefaultIncludeEntityViewPanel.java
@@ -1,12 +1,15 @@
 package org.web4thejob.web.panel;
 
 import org.springframework.context.annotation.Scope;
+import org.web4thejob.command.Command;
 import org.web4thejob.command.CommandEnum;
 import org.web4thejob.orm.Entity;
 import org.web4thejob.web.panel.base.AbstractMutablePanel;
 import org.zkoss.bind.BindComposer;
+import org.zkoss.bind.annotation.BindingParam;
 import org.zkoss.zk.ui.Component;
 import org.zkoss.zk.ui.Executions;
+import org.zkoss.zk.ui.util.Clients;
 
 import java.util.Set;
 
@@ -60,7 +63,9 @@ public class DefaultIncludeEntityViewPanel extends AbstractMutablePanel implemen
     protected void arrangeForMutableMode() {
         if (bc != null) return;
 
-        Component root = Executions.createComponentsDirectly("<include src=\"sec/include.zul\"/>", null, ((org.zkoss.zul.Panel) base).getPanelchildren(), null);
+        ((org.zkoss.zul.Panel) base).getPanelchildren().setStyle("overflow: auto;");
+        Component root = Executions.createComponentsDirectly("<include src=\"sec/include.zul\"/>", null,
+                ((org.zkoss.zul.Panel) base).getPanelchildren(), null);
 
         bc = new BindComposer<>();
         bc.setViewModel(this);
@@ -77,6 +82,17 @@ public class DefaultIncludeEntityViewPanel extends AbstractMutablePanel implemen
         }
 */
 
+    }
+
+    @org.zkoss.bind.annotation.Command
+    public void processCommand(@BindingParam("name") String name) {
+        Clients.alert("command clicked: " + name);
+
+        for (Command command : getCommands()) {
+            if (command.getId().name().equals(name)) {
+                command.process();
+            }
+        }
     }
 
     @Override
